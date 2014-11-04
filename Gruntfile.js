0ab6faec68436cf11307d06f394e7c0cfc4a89e4
kgentner <gentnerkarl@gmail.com>
2014-11-04 15:17:59 -0800

    removed contrib-jscs from npm load

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-jscs');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-jscs');
 
