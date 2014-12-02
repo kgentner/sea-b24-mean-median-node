b041690193eb3d36151e2f8181bf2c601a9d5768
kgentner <gentnerkarl@gmail.com>
2014-12-01 17:21:33 -0800

    fixed linting issues

--- test/mean_median_mode_tests.js
+++ test/mean_median_mode_tests.js
@@ -5,25 +5,18 @@ var mmm = require('../lib/mean_median_mode');
 
 describe('Mean', function() {
   it('gets an accurate mean', function() {
-    expect(mmm.mean([2,4,6])).to.eql(4);
+    expect(mmm.mean([2, 4, 6])).to.eql(4);
   });
 });
 
 describe('Median', function() {
   it('gets an accurate median', function() {
-    expect(mmm.median([4,5,6])).to.eql(5);
+    expect(mmm.median([4, 5, 6])).to.eql(5);
   });
 });
 
 describe('Mode', function() {
   it('gets an accurae mode', function() {
-  expect(mmm.mode([5,5,6,7])).to.eql(5);
-// <<<<<<< HEAD //this is the stuff that we had
-//   it('gets an accurae mode', function() {
-//     expect(mmm.mode([5,5,6,7])).to.eql(5);
-// ======= //this is the stuff that was upstream
-//   it('gets an accurate mode', function() {
-//     expect(mmm.mode(5,5,6,7)).to.eql(5);
-// >>>>>>> bb9fe852322d2b0df5279091659a53bb1271be69
+    expect(mmm.mode([5, 5, 6, 7])).to.eql(5);
   });
 });
