988b7ef9aeb5d8ae75b31b07eac443be80d7922c
kgentner <gentnerkarl@gmail.com>
2018-01-21 16:15:31 -0800

    updates to controller, service, and test

---
+++ app/js/controllers/mmm_controller_with_http.js
@@ -0,0 +1,19 @@
+'use strict';
+/*thanks to S. Burbidge, J. Elsey, S. Lingwood, & C. Renwick for help*/
+
+module.exports = function(app) {
+  app.controller('mmmCtrl', ['$scope', '$http', 'ResourceBackend',
+    function($scope, $http, ResourceBackend) {
+
+      var mmmBackend = new ResourceBackend();
+
+      $scope.calcMMM = function() {
+        mmmBackend.postMMM ($scope.numInput.split(' '))
+        .success(function(data) {
+          $scope.mean = data.mean;
+          $scope.median = data.median;
+          $scope.mode = data.mode;
+        });
+      };
+    }]);
+};
---
+++ server_withpost.js
@@ -0,0 +1,53 @@
+'use strict';
+/*thanks to Joe Elsey, Stephanie Lingwood, & Charles Renwick for help */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var meanFunc = require('./app/js/mean');
+var medianFunc = require('./app/js/median');
+var modeFunc = require('./app/js/mode');
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+app.use(bodyParser.json());
+
+app.post('/api/mmm', function(req, res) {
+
+  var mean;
+  var median;
+  var mode;
+  var numArray = [];
+  var isValidArray = true;
+
+  for (var i = 0; i < req.body.numList.length; i++) {
+    if (isNaN(parseFloat(req.body.numList[i]))) {
+      isValidArray = false;
+    }
+    numArray.push(Number(req.body.numList[i]));
+  }
+
+  //initially numList is an empty array, but since this app is
+  //constantly assessing upon changes,
+  //the numList becomes [''] when the input area is manually cleared.
+  //The first conditional tests for this.
+  if (req.body.numList[0] === '') {
+    mean = null;
+    median = null;
+    mode = null;
+  } else if (isValidArray) {
+    mean = meanFunc(numArray);
+    median = medianFunc(numArray);
+    mode = modeFunc(numArray);
+  } else { //if any element of the array is not a number
+    mean = 'invalid input';
+    median = 'invalid input';
+    mode = 'invalid input';
+  }
+
+  res.json({mean: mean, median: median, mode: mode});
+});
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('server running on port: %d', app.get('port'));
+});
